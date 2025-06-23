40cdb692cf6b5ed7a1a885f626bd75e899078beb
Himawari Lad <Sacrificial.animal@gmail.com>
2025-06-23 18:31:02 +0300

    Code added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       //CASE1//
+        System.out.println("CASE 1");
+        ////////////////////////////
+        int [] massOne = new int [3];
+        massOne[0] = 1;
+        massOne[1] = 2;
+        massOne[2] = 3;
+        ////////////////////////////
+        float [] massTwo = {1.57F, 7.654F, 9.986F};
+        ////////////////////////////
+        byte [] massThree = {4, 86, 116};
+        ////////////////////////////
+        System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//Задание 2, 3 можно сделать через Arrays, но в задании показан пример без квадратных скобок.
+
+        //CASE2//
+        System.out.println("CASE 2");
+        System.out.print(massOne[0] + ", " );
+        System.out.print(massOne[1] + ", " );
+        System.out.print(massOne[2]);
+        /////////////////////////////
+        System.out.println();
+        System.out.print(massTwo[0] + ", " );
+        System.out.print(massTwo[1] + ", " );
+        System.out.print(massTwo[2]);
+        /////////////////////////////
+        System.out.println();
+        System.out.print(massThree[0] + ", " );
+        System.out.print(massThree[1] + ", " );
+        System.out.print(massThree[2]);
+        /////////////////////////////
+        System.out.println();
+        System.out.println();
+
+        //CASE3//
+        System.out.println("CASE 3");
+        System.out.print(massOne[2] + ", " );
+        System.out.print(massOne[1] + ", " );
+        System.out.print(massOne[0]);
+        /////////////////////////////
+        System.out.println();
+        System.out.print(massTwo[2] + ", " );
+        System.out.print(massTwo[1] + ", " );
+        System.out.print(massTwo[0]);
+        /////////////////////////////
+        System.out.println();
+        System.out.print(massThree[2] + ", " );
+        System.out.print(massThree[1] + ", " );
+        System.out.print(massThree[0]);
+        /////////////////////////////
+        System.out.println();
+        System.out.println();
+
+        //CASE4//
+        System.out.println("CASE 4");
+        for (int i = 0; i < massOne.length; i++) {
+            if (massOne[i] % 2 != 0);
+            massOne[i] = massOne[i] + 1;
         }
+        System.out.println(Arrays.toString(massOne));
     }
 }
